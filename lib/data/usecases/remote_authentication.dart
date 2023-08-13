a1d67b8f183534c34d8a278b7432ed2896d46953
Igor Atilar <igoratilar10@gmail.com>
2023-08-13 20:53:37 -0300

    refactor: move production code to their own target

---
+++ lib/data/usecases/remote_authentication.dart
@@ -0,0 +1,20 @@
+import 'package:meta/meta.dart';
+
+import '../../domain/usecases/authentication.dart';
+
+import '../http/http.dart';
+
+class RemoteAuthentication {
+  final HttpClient httpClient;
+  final String url;
+
+  Future<void> auth(AuthenticationParams params) async {
+    await httpClient.request(
+      url: url,
+      method: 'post',
+      body: params.toJson(),
+    );
+  }
+
+  RemoteAuthentication({@required this.httpClient, @required this.url});
+}
