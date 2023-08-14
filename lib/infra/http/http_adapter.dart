37fa9acfb75b967d69672f0fe11f511e81a2989c
Igor Atilar <igoratilar10@gmail.com>
2023-08-14 16:55:21 -0300

    test(http_adapter): ensure HttpAdapter throws ServerError if invalid method is provided

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -21,7 +21,11 @@ class HttpAdapter implements HttpClient {
       'accept': 'application/json',
     };
 
-    final response = await client.post(url, headers: headers, body: jsonBody);
+    var response = Response('', 500);
+
+    if (method == 'post') {
+      response = await client.post(url, headers: headers, body: jsonBody);
+    }
 
     return _handleResponse(response);
   }
