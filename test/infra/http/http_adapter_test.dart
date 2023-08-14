f8d91617863816563a0a28e8b2967b4b181b56e3
Igor Atilar <igoratilar10@gmail.com>
2023-08-14 15:35:06 -0300

    test(http_adapter): ensure HttpAdapter calls post without body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -16,12 +16,14 @@ class HttpAdapter {
     @required String method,
     Map body,
   }) async {
+    final jsonBody = body != null ? jsonEncode(body) : null;
+
     final headers = {
       'content-type': 'application/json',
       'accept': 'application/json',
     };
 
-    await client.post(url, headers: headers, body: jsonEncode(body));
+    await client.post(url, headers: headers, body: jsonBody);
   }
 }
 
@@ -48,6 +50,12 @@ void main() {
             'accept': 'application/json',
           },
           body: '{"any_key":"any_value"}'));
+    });
+
+    test('Should call post without body', () async {
+      await sut.request(url: url, method: 'post');
+
+      verify(client.post(any, headers: anyNamed('headers')));
     });
   });
 }
