70b7c19c578f4843f92d5df42915e7b9a7bb278c
Igor Atilar <igoratilar10@gmail.com>
2023-08-14 14:04:15 -0300

    feat: add http error handler on RemoteAuthentication

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -1,6 +1,7 @@
 import 'package:meta/meta.dart';
 
 import '../../domain/usecases/authentication.dart';
+import '../../domain/helpers/helpers.dart';
 
 import '../http/http.dart';
 
@@ -10,12 +11,15 @@ class RemoteAuthentication {
 
   Future<void> auth(AuthenticationParams params) async {
     final body = RemoteAuthenticationParams.fromDomain(params).toJson();
-
-    await httpClient.request(
-      url: url,
-      method: 'post',
-      body: body,
-    );
+    try {
+      await httpClient.request(
+        url: url,
+        method: 'post',
+        body: body,
+      );
+    } on HttpError {
+      throw DomainError.unexpected;
+    }
   }
 
   RemoteAuthentication({@required this.httpClient, @required this.url});
